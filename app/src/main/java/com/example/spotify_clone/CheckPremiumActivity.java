96dd685fdf45eccd5e2dbd773151a7b201086a15
ThienHoanNeee <softteam2021@gmail.com>
2022-04-29 10:07:45 +0700

    [Update] add check premium activity

---
+++ app/src/main/java/com/example/spotify_clone/CheckPremiumActivity.java
@@ -0,0 +1,14 @@
+package com.example.spotify_clone;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheckPremiumActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premium_check);
+    }
+}
